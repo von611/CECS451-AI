f91751839e4dfdd1f48246ec213a8fd0112f5745
von95 <74983929+von95@users.noreply.github.com>
2020-11-24 10:51:18 -0800

    Add files via upload

---
+++ Assignment3-Hill_and_Genetic_Algo/board.py
@@ -0,0 +1,47 @@
+import random
+import numpy as np
+
+
+class Board:
+    def __init__(self, n):
+        self.n_queen = n
+        self.map = [[0 for j in range(n)] for i in range(n)]
+        self.fit = 0
+    
+        for i in range(self.n_queen):
+            j = random.randint(0, self.n_queen - 1)
+            self.map[i][j] = 1
+
+    def fitness(self):        
+        for i in range(self.n_queen):
+            for j in range(self.n_queen):
+                if self.map[i][j] == 1:
+                    for k in range(1, self.n_queen - i):
+                        if self.map[i + k][j] == 1:
+                            self.fit += 1
+                        if j - k >= 0 and self.map[i + k][j - k] == 1:
+                            self.fit += 1
+                        if j + k < self.n_queen and self.map[i + k][j + k] == 1:
+                            self.fit += 1
+
+    def show(self):
+        print(np.matrix(self.map))
+        print("Fitness: ",  self.fit)
+
+    def flip(self, i, j):
+        if self.map[i][j] == 0:
+            self.map[i][j] = 1
+        else:
+            self.map[i][j] = 0
+
+    def get_map(self):
+        return self.map
+    
+    def get_fit(self):
+        return self.fit
+
+            
+if __name__ == '__main__':
+    test = Board(5)
+    test.fitness()
+    test.show()    
